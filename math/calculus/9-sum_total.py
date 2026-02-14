9e958ea1a97ec43c6ea252cc5eadde1b27865b42
Etienne Kagaba <156916057+etienne-kagaba@users.noreply.github.com>
2026-02-14 21:56:30 +0200

    Create 9-sum_total.py

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+Calculates the sum of squares
+'''
+
+
+def summation_i_squared(n):
+    """
+    Calculate the sum of squares
+    
+    Args:
+        n (integer): the stopping number
+    Returns:
+        int: if n is a valid integer
+        None: if n is not a valid integer
+    """
+    if !isinstance(n, int):
+        return None
+    
+    return (n * (n + 1) * (2 * n + 1))//6
