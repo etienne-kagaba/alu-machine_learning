d64697ea11d61eb7adff8046f50afa96bf642d73
Etienne Kagaba <156916057+etienne-kagaba@users.noreply.github.com>
2026-02-14 21:57:43 +0200

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -14,7 +14,7 @@ def summation_i_squared(n):
         int: if n is a valid integer
         None: if n is not a valid integer
     """
-    if !isinstance(n, int):
+    if not isinstance(n, int):
         return None
     
     return (n * (n + 1) * (2 * n + 1))//6
